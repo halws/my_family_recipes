9d3ecb849a33c4ae1c5892ff74614f87808f9de8
Nazar Kotsyuba <kotsuyba@gmail.com>
2021-04-02 06:31:42 +0200

    show empty appbar

--- lib/screens/history/history_screen.dart
+++ lib/screens/history/history_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:my_family_recipes/mixins/ToggleIngredientsVisibilityMixin.dart';
 import 'package:my_family_recipes/providers/history.dart';
-import 'package:my_family_recipes/screens/basket/components/toggle-ingredients-visibility.dart';
 import 'package:my_family_recipes/utils/getColorFromHex.dart';
+import 'package:my_family_recipes/widgets/AppbarWithoutSwither.dart';
 import 'package:provider/provider.dart';
 
 class HistoryScreen extends StatefulWidget {
@@ -12,8 +11,7 @@ class HistoryScreen extends StatefulWidget {
   _HistoryScreenState createState() => _HistoryScreenState();
 }
 
-class _HistoryScreenState extends State<HistoryScreen>
-    with ToggleIngredientsVisibility {
+class _HistoryScreenState extends State<HistoryScreen> {
   bool _isInit = true;
   bool _isLoading = false;
 
@@ -37,10 +35,7 @@ class _HistoryScreenState extends State<HistoryScreen>
       backgroundColor: ColorUtils.hexToColor('#F8FBFE'),
       appBar: PreferredSize(
         preferredSize: Size(double.infinity, 110),
-        child: AppBarWithSwitcher(
-          isSwitched: isSwitched,
-          toggleVisibility: toggleVisibility,
-        ),
+        child: AppBarWithoutSwitcher(),
       ),
       body: SafeArea(
         child: Container(
